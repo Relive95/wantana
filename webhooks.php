bd45f6a4a0445e385d1fd12f9946d09fa12f22f8
Relive95 <relive95@gmail.com>
2021-02-04 18:23:02 +0700

    webhooks

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = '5o05EX0TrUOd/7U1ZyW5FUFMkCXgOipLkZaXa2IpE85WR9IJgQ9xflvOM/9EozfB1J+tTDzq9A1E0KuEO1qysCiGFTX6cK7OegDFv6EEnPOm0jDn/eF+bR2Y9ykeUF1O+ed8WJ5YOii9YCBW3vFX/QdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
