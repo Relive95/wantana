17a10604dc274987e8ebb54e1531039fbede8077
Relive95 <relive95@gmail.com>
2021-02-04 18:41:27 +0700

    'tangsi'

--- webhooks.php
+++ webhooks.php
@@ -23,7 +23,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'ควาย'
+				'text' => 'รถถัง'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
